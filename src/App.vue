a2235ca6e053acdbaa97dad3e00879a3d0289841
Igor Ivanov <igorpi25@gmail.com>
2021-01-26 18:09:44 +0900

    fix grammar in description

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col p-8 justify-center items-center">
       <h1 class="text-2xl uppercase">sakha-accords.com</h1>
       <p>
-        Здесь будут аккорды песен на Якуcском языке
+        Здесь будут аккорды песен на Якутском языке
       </p>
     </div>
     
