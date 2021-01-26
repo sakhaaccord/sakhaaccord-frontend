7a8cdc327563af1bc35c9931418fdf038403a7b3
Igor Ivanov <igorpi25@gmail.com>
2021-01-26 17:58:46 +0900

    refactor by move logic of HelloWorld to App

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Sakha-accord"/>
+    <h1>sakha-accords.com</h1>
+    <p>
+      Здесь будут аккорды песен на Якутском языке
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
