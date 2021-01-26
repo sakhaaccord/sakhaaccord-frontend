d5362bb83e44d095f49363db4a3ed86398c100f9
Igor Ivanov <igorpi25@gmail.com>
2021-01-26 18:08:32 +0900

    update styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <h1>sakha-accords.com</h1>
-    <p>
-      Здесь будут аккорды песен на Якутском языке
-    </p>
+    <div class="flex flex-col p-8 justify-center items-center">
+      <h1 class="text-2xl uppercase">sakha-accords.com</h1>
+      <p>
+        Здесь будут аккорды песен на Якуcском языке
+      </p>
+    </div>
+    
   </div>
 </template>
 
